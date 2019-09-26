f9aa7b99d4fe29a9863fef01468d7804f78e43aa
manohar52 <46411996+manohar52@users.noreply.github.com>
2019-09-25 20:58:17 -0500

    Log out & Username display

--- app/src/main/java/com/example/facilityreservationsystem/AdminHomeScreen.java
+++ app/src/main/java/com/example/facilityreservationsystem/AdminHomeScreen.java
@@ -1,8 +1,15 @@
 package com.example.facilityreservationsystem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import model.SysUser;
 
 public class AdminHomeScreen extends AppCompatActivity {
 
@@ -11,4 +18,25 @@ public class AdminHomeScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home_screen);
     }
+    private SysUser currSysUser;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.action_logout:
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
